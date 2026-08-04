c2a3b8c21bd3173c508c57a2f84ba6772f6ade30
AugustoDevZ <ruiton2025@gmail.com>
2026-08-04 10:38:38 -0500

    Correciòn de dependencias y cambio de nombre al proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.mineide {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.mineide to javafx.fxml;
+    exports org.example.mineide;
+}
