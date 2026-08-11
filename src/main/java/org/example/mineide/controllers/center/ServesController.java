272d5deeadbfd13274e87fb5b43756ac57720c26
lord.orion <lorenzoolivera130530@gmail.com>
2026-08-10 23:33:08 -0300

    implement a parcial function for a downoald software of server for create servidors of evething version

--- src/main/java/org/example/mineide/controllers/center/ServesController.java
+++ src/main/java/org/example/mineide/controllers/center/ServesController.java
@@ -1,8 +1,25 @@
 package org.example.mineide.controllers.center;
 
+import com.almasb.fxgl.net.Client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.http.*;
+import java.net.URI;
+
+
 public class ServesController {
+    void directoryCreate(String software, String version) throws IOException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://lrweb.dpdns.org/mcDowload"))
+                .header("software", software)
+                .header("version", version)
+                .GET()
+                .build();
+        HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+    }
 }
