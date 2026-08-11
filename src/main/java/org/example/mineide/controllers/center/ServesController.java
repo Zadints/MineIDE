f4436f58ca1d933f21377a7ec15a0e47e5b2cd83
lord.orion <lorenzoolivera130530@gmail.com>
2026-08-10 23:33:51 -0300

    implement a parcial function for a downoald software of server for create servidors of evething version

--- src/main/java/org/example/mineide/controllers/center/ServesController.java
+++ src/main/java/org/example/mineide/controllers/center/ServesController.java
@@ -1,17 +1,12 @@
 package org.example.mineide.controllers.center;
 
-import com.almasb.fxgl.net.Client;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.http.*;
 import java.net.URI;
 
 
 public class ServesController {
-    void directoryCreate(String software, String version) throws IOException {
+    void directoryCreate(String software, String version) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
