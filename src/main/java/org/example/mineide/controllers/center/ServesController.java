2d9b72eaba16660b4ecfb1be496b58c519108bc2
lord.orion <lorenzoolivera130530@gmail.com>
2026-08-14 01:18:28 -0300

    advance the system of directories

--- src/main/java/org/example/mineide/controllers/center/ServesController.java
+++ src/main/java/org/example/mineide/controllers/center/ServesController.java
@@ -2,6 +2,8 @@ package org.example.mineide.controllers.center;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.scene.Node;
@@ -13,6 +15,10 @@ import java.io.File;
 public class ServesController {
     Path path;
     @FXML
+    private TextField pathfield;
+    @FXML
+    private TextField namefield;
+    @FXML
     void directoryselect(ActionEvent event){
         DirectoryChooser carpeta = new DirectoryChooser();
         carpeta.setTitle("select main directory");
@@ -22,9 +28,13 @@ public class ServesController {
         File file = carpeta.showDialog(stage);
         path = file.toPath();
         System.out.println(path);
+        pathfield.setText(path.toString());
     }
-    void serverCreate(String software, String version, String path) throws Exception {
+    void serverCreate(ActionEvent event) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
+
+        String software = "hola";
+        String version = "no";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://lrweb.dpdns.org/mcDowload"))
@@ -35,10 +45,13 @@ public class ServesController {
         HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
         if (response.statusCode() == 200){
             if (path == null){
-                Files.write(Paths.get( System.getProperty("user.home") + File.separator + "Desktop"), response.body());
+                Path name = Paths.get(System.getProperty("user.home") + File.separator + "Desktop");
+                Path main = Files.createDirectory(name.resolve(pathfield.getText()));
+                Files.write(main.resolve("software.jar"), response.body());
 
             }else {
-                Files.write(Paths.get(path), response.body());
+                Path name = Files.createDirectory(path.resolve(namefield.getText()));
+                Files.write(name.resolve("software.jar") , response.body());
             }
         }
 
