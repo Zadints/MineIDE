22e55012217e9673178f733d142f04128ffeced6
lord.orion <lorenzoolivera130530@gmail.com>
2026-08-13 22:53:11 -0300

    update system of select directory for create server

--- src/main/java/org/example/mineide/controllers/center/ServesController.java
+++ src/main/java/org/example/mineide/controllers/center/ServesController.java
@@ -1,13 +1,29 @@
 package org.example.mineide.controllers.center;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 import java.net.http.*;
 import java.net.URI;
 import java.nio.file.*;
 import java.io.File;
 
 public class ServesController {
-    void directoryCreate(String software, String version, String path) throws Exception {
+    Path path;
+    @FXML
+    void directoryselect(ActionEvent event){
+        DirectoryChooser carpeta = new DirectoryChooser();
+        carpeta.setTitle("select main directory");
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        File file = carpeta.showDialog(stage);
+        path = file.toPath();
+        System.out.println(path);
+    }
+    void serverCreate(String software, String version, String path) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
@@ -22,7 +38,7 @@ public class ServesController {
                 Files.write(Paths.get( System.getProperty("user.home") + File.separator + "Desktop"), response.body());
 
             }else {
-                Files.write(Paths.get("pedroluis.txt"), response.body());
+                Files.write(Paths.get(path), response.body());
             }
         }
 
